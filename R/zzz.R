88fc1c48dfbc135040785802d67bd33fde8507a6
William Asquith <william.asquith@ttu.edu>
2013-12-03 00:00:00 +0000

    version 0.23

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,9 @@
-".First.lib" <-
+".onAttach" <-
 function(lib, pkg) {
-  MSG <- packageStartupMessage;   # renaming the function
-  dsc <- packageDescription(pkg); # function will not be found
-  # without loading the utils package in the
-  # Depends: section of the DESCRIPTION
+  MSG <- packageStartupMessage # renaming the function
+  dsc <- packageDescription(pkg) # function will not be found without
+  # loading the utils package in the Depends: section of the DESCRIPTION
   if(interactive() || getOption("verbose")) {
-    MSG(sprintf("Hydraulics package %s (%s) loaded.", pkg, dsc$Version));
+    MSG(sprintf("# Hydraulics package %s (%s) loaded.", pkg, dsc$Version))
   }
 }
